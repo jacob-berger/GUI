aa24ca485397b9db82d48973fc30dd267ac349a7
Jacob Berger <jberger.1919@gmail.com>
2020-03-10 15:17:12 -0700

    Working on adding second level

--- jbergerAshman/src/jbergerAshman/jbergerAshman.java
+++ jbergerAshman/src/jbergerAshman/jbergerAshman.java
@@ -1,10 +1,8 @@
 package jbergerAshman;
 
-import com.sun.xml.internal.ws.policy.EffectiveAlternativeSelector;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -13,15 +11,12 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
 import java.util.Random;
@@ -43,14 +38,14 @@ public class jbergerAshman extends Application {
     boolean run;
     Image ghost1, ghost2, ghost3, ghost4;
     int[] ghost1location, ghost2location, ghost3location, ghost4location;
-    long previousTime = System.currentTimeMillis();
+    long previousPlayerTime = System.currentTimeMillis();
+    long previousGhostTime = System.currentTimeMillis();
     GameBoard gameBoard;
-    private boolean paused;
-    private AnimationTimer timer;
-
     boolean goNorth, goEast, goSouth, goWest;
-
     Line line;
+    private boolean paused;
+    private AnimationTimer playerTimer, ghostTimer;
+    BorderPane root;
 
     public static void main(String[] args) {
         launch(args);
@@ -114,11 +109,11 @@ public class jbergerAshman extends Application {
         this.cakesEaten = cakesEaten;
     }
 
-    public int getRemaining() {
+    public int getCakesRemaining() {
         return remaining;
     }
 
-    public void setRemaining(int remaining) {
+    public void setCakesRemaining(int remaining) {
         this.remaining = remaining;
     }
 
@@ -133,22 +128,20 @@ public class jbergerAshman extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        BorderPane root = new BorderPane();
+        root = new BorderPane();
 
         gameBoard = new GameBoard();
         //224 cakes total including where player starts
-        setRemaining(223);
+        setCakesRemaining(223);
         setCakesEaten(1);
         setNumGhosts(2);
         setLevel(1);
         setPaused(false);
         playerLocation = new int[]{0, 0};
         player = new Image("Images/Player.png", 20, 20, false, false);
-        Circle circle = new Circle(10, ash);
-        circle.setFill(ash);
-        gameBoard.tilePane.getChildren().set(0, circle);
-        createGhosts();
 
+        createGhosts();
+        setGhostSpeed(1);
 
         root.setCenter(gameBoard);
 
@@ -159,96 +152,351 @@ public class jbergerAshman extends Application {
         root.setBottom(toolBar);
         Scene scene = new Scene(root, width, height);
 
-        //Timer?
-        Button button = new Button("Start timer");
-        timer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                onTimer(now);
-                if (goNorth) setStatus("North");
-                if (goEast) setStatus("East");
-                if (goSouth) setStatus("South");
-                if (goWest) setStatus("West");
-
-                movePlayer();
-            }
-        };
-
-        button.setOnAction(actionEvent -> timer.start());
         HBox bottomBox = new HBox();
-        line = new Line();
-        line.setStrokeWidth(20);
-        bottomBox.getChildren().addAll(button, toolBar, line);
+        bottomBox.getChildren().addAll(toolBar);
 
         root.setBottom(bottomBox);
 
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                switch (event.getCode()) {
-                    case UP: goNorth = true; break;
-                    case RIGHT: goEast = true; break;
-                    case DOWN: goSouth = true; break;
-                    case LEFT: goWest = true; break;
-                }
+        scene.setOnKeyPressed(event -> {
+            switch (event.getCode()) {
+                case UP:
+                    goNorth = true;
+                    goSouth = false;
+                    break;
+                case RIGHT:
+                    goEast = true;
+                    goWest = false;
+                    break;
+                case DOWN:
+                    goNorth = false;
+                    goSouth = true;
+                    break;
+                case LEFT:
+                    goEast = false;
+                    goWest = true;
+                    break;
             }
         });
-
-        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                switch (event.getCode()) {
-                    case UP:    goNorth = false; break;
-                    case DOWN:  goSouth = false; break;
-                    case LEFT:  goWest  = false; break;
-                    case RIGHT: goEast  = false; break;
-                }
-            }
-        });
-
 
         //Hopefully just for testing
         primaryStage.setResizable(false);
 
+        playerTimer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (!getPaused()) {
+                    if (getCakesRemaining() == 0) {
+                        nextLevel();
+                    }
+                    onPlayerTimer(now);
+                }
+            }
+        };
+        playerTimer.start();
 
-        primaryStage.setTitle("Hello World!");
+        ghostTimer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (!getPaused()) {
+                    onGhostTimer(now);
+                }
+            }
+        };
+
+        ghostTimer.start();
+
+        primaryStage.setTitle("Jacob Berger Ashman");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    private void nextLevel() {
+        setLevel(2);
+        setGhostSpeed(2);
+        setCakesEaten(224);
+        setCakesRemaining(224);
+        setNumGhosts(4);
+        gameBoard = new GameBoard();
+        playerLocation = new int[]{0, 0};
+        player = new Image("Images/Player.png", 20, 20, false, false);
+        createGhosts();
+
+        playerTimer.stop();
+        ghostTimer.stop();
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Level 1 passed!");
+        alert.setContentText("Press OK to continue...");
+//        alert.setOnHidden();
+        alert.show();
+
+        root.setCenter(gameBoard);
+
+        playerTimer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (!getPaused()) {
+                    if (getCakesRemaining() == 0) {
+                        //gameWon();
+                    }
+                    onPlayerTimer(now);
+                }
+            }
+        };
+        playerTimer.start();
+
+        ghostTimer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (!getPaused()) {
+                    onGhostTimer(now);
+                }
+            }
+        };
+
+        setPaused(false);
+    }
+
+    private void onGhostTimer(long now) {
+        now = System.currentTimeMillis();
+        long elapsed = (now - previousGhostTime);
+        if (elapsed >= 200 / getGhostSpeed()) {
+            previousGhostTime = now;
+            moveGhost(1);
+            moveGhost(2);
+            if (getNumGhosts() > 2) {
+                moveGhost(3);
+                moveGhost(4);
+            }
+        }
+    }
+
     private void movePlayer() {
         if (goNorth) {
-            playerLocation[0] -= 1;
+            if (playerLocation[0] != 0) {
+                if (gameBoard.board[playerLocation[0] - 1][playerLocation[1]] != Tile.WALL) {
+                    playerLocation[0] -= 1;
+                    if (gameBoard.board[playerLocation[0]][playerLocation[1]] == Tile.CAKE) {
+                        setCakesEaten(getCakesEaten() + 1);
+                        setCakesRemaining(getCakesRemaining() - 1);
+                        setNodeAtPlayer(Tile.EMPTY);
+                    }
+                    gameBoard.board[playerLocation[0]][playerLocation[1]] = Tile.EMPTY;
+                    setNodeAtLocation(playerLocation[0] + 1, playerLocation[1], new Image("Images/Empty.png", 20, 20, false, false));
+                } else {
+                    goNorth = false;
+                }
+            }
         }
         if (goEast) {
-            playerLocation[1] += 1;
+            if (playerLocation[1] != 19) {
+                if (gameBoard.board[playerLocation[0]][playerLocation[1] + 1] != Tile.WALL) {
+                    playerLocation[1] += 1;
+                    if (gameBoard.board[playerLocation[0]][playerLocation[1]] == Tile.CAKE) {
+                        setCakesEaten(getCakesEaten() + 1);
+                        setCakesRemaining(getCakesRemaining() - 1);
+                        setNodeAtPlayer(Tile.EMPTY);
+                    }
+                    gameBoard.board[playerLocation[0]][playerLocation[1]] = Tile.EMPTY;
+                    setNodeAtLocation(playerLocation[0], playerLocation[1] - 1, new Image("Images/Empty.png", 20, 20, false, false));
+                } else {
+                    goEast = false;
+                }
+            }
         }
         if (goSouth) {
-            playerLocation[0] += 1;
+            if (playerLocation[0] != 19) {
+                if (gameBoard.board[playerLocation[0] + 1][playerLocation[1]] != Tile.WALL) {
+                    playerLocation[0] += 1;
+                    if (gameBoard.board[playerLocation[0]][playerLocation[1]] == Tile.CAKE) {
+                        setCakesEaten(getCakesEaten() + 1);
+                        setCakesRemaining(getCakesRemaining() - 1);
+                        setNodeAtPlayer(Tile.EMPTY);
+                    }
+                    gameBoard.board[playerLocation[0]][playerLocation[1]] = Tile.EMPTY;
+                    setNodeAtLocation(playerLocation[0] - 1, playerLocation[1], new Image("Images/Empty.png", 20, 20, false, false));
+                } else {
+                    goSouth = false;
+                }
+            }
         }
         if (goWest) {
-            playerLocation[1] -= 1;
+            if (playerLocation[1] != 0) {
+                if (gameBoard.board[playerLocation[0]][playerLocation[1] - 1] != Tile.WALL) {
+                    playerLocation[1] -= 1;
+                    if (gameBoard.board[playerLocation[0]][playerLocation[1]] == Tile.CAKE) {
+                        setCakesEaten(getCakesEaten() + 1);
+                        setCakesRemaining(getCakesRemaining() - 1);
+                        setNodeAtPlayer(Tile.EMPTY);
+                    }
+                    gameBoard.board[playerLocation[0]][playerLocation[1]] = Tile.EMPTY;
+                    setNodeAtLocation(playerLocation[0], playerLocation[1] + 1, new Image("Images/Empty.png", 20, 20, false, false));
+                } else {
+                    goWest = false;
+                }
+            }
         }
 
         drawPlayer();
+    }
+
+    private void moveGhost(int ghostNumber) {
+        Image ghost;
+        int[] ghostLocation = new int[2];
+        switch (ghostNumber) {
+            case 1:
+                ghost = ghost1;
+                ghostLocation = ghost1location;
+                break;
+            case 2:
+                ghost = ghost2;
+                ghostLocation = ghost2location;
+                break;
+            case 3:
+                ghost = ghost3;
+                ghostLocation = ghost3location;
+                break;
+            case 4:
+                ghost = ghost4;
+                ghostLocation = ghost4location;
+                break;
+        }
+
+        Random random = new Random();
+        int direction = random.nextInt(4);
+        if (direction == 0) {
+            if (ghostLocation[0] != 0) {
+                if (gameBoard.board[ghostLocation[0] - 1][ghostLocation[1]] != Tile.WALL) {
+                    if (gameBoard.board[ghostLocation[0] - 1][ghostLocation[1]] == Tile.EMPTY) {
+                        ghostLocation[0] -= 1;
+                        setNodeAtLocation(ghostLocation[0] + 1, ghostLocation[1], new Image("Images/Empty.png", 20, 20, false, false));
+                    } else {
+                        ghostLocation[0] -= 1;
+                        setNodeAtLocation(ghostLocation[0] + 1, ghostLocation[1], new Image("Images/Cake.png", 20, 20, false, false));
+                    }
+                }
+            }
+        }
+        if (direction == 1) {
+            if (ghostLocation[1] != 19) {
+                if (gameBoard.board[ghostLocation[0]][ghostLocation[1] + 1] != Tile.WALL) {
+                    if (gameBoard.board[ghostLocation[0]][ghostLocation[1] + 1] == Tile.EMPTY) {
+                        ghostLocation[1] += 1;
+                        setNodeAtLocation(ghostLocation[0], ghostLocation[1] - 1, new Image("Images/Empty.png", 20, 20, false, false));
+                    } else {
+                        ghostLocation[1] += 1;
+                        setNodeAtLocation(ghostLocation[0], ghostLocation[1] - 1, new Image("Images/Cake.png", 20, 20, false, false));
+                    }
+                }
+            }
+        }
+        if (direction == 2) {
+            if (ghostLocation[0] != 19) {
+                if (gameBoard.board[ghostLocation[0] + 1][ghostLocation[1]] != Tile.WALL) {
+                    if (gameBoard.board[ghostLocation[0] + 1][ghostLocation[1]] == Tile.EMPTY) {
+                        ghostLocation[0] += 1;
+                        setNodeAtLocation(ghostLocation[0] - 1, ghostLocation[1], new Image("Images/Empty.png", 20, 20, false, false));
+                    } else {
+                        ghostLocation[0] += 1;
+                        setNodeAtLocation(ghostLocation[0] - 1, ghostLocation[1], new Image("Images/Cake.png", 20, 20, false, false));
+                    }
+                }
+            }
+        }
+        if (direction == 3) {
+            if (ghostLocation[1] != 0) {
+                if (gameBoard.board[ghostLocation[0]][ghostLocation[1] - 1] != Tile.WALL) {
+                    if (gameBoard.board[ghostLocation[0]][ghostLocation[1] - 1] == Tile.EMPTY) {
+                        ghostLocation[1] -= 1;
+                        setNodeAtLocation(ghostLocation[0], ghostLocation[1] + 1, new Image("Images/Empty.png", 20, 20, false, false));
+                    } else {
+                        ghostLocation[1] -= 1;
+                        setNodeAtLocation(ghostLocation[0], ghostLocation[1] + 1, new Image("Images/Cake.png", 20, 20, false, false));
+                    }
+                }
+            }
+        }
+
+        switch (ghostNumber) {
+            case 1:
+                ghost1location = ghostLocation;
+                break;
+            case 2:
+                ghost2location = ghostLocation;
+                break;
+            case 3:
+                ghost3location = ghostLocation;
+                break;
+            case 4:
+                ghost4location = ghostLocation;
+                break;
+        }
+
+        drawGhosts();
     }
 
     private void drawPlayer() {
         setNodeAtLocation(playerLocation[0], playerLocation[1], player);
     }
 
-    private void rotate() {
-        Rotate rotate = new Rotate();
-        rotate.setAngle(1.5);
-        line.getTransforms().add(rotate);
+    private void drawGhosts() {
+        setNodeAtLocation(ghost1location[0], ghost1location[1], ghost1);
+        setNodeAtLocation(ghost2location[0], ghost2location[1], ghost2);
+        if (getNumGhosts() > 2) {
+            setNodeAtLocation(ghost3location[0], ghost3location[1], ghost3);
+            setNodeAtLocation(ghost4location[0], ghost4location[1], ghost4);
+        }
     }
 
-    private void onTimer(long now) {
+    private void onPlayerTimer(long now) {
         now = System.currentTimeMillis();
-        long elapsed = (now - previousTime);
-        previousTime = now;
-        setStatus("" + elapsed);
-        rotate();
+        boolean ghostCollision = false;
+        long elapsed = (now - previousPlayerTime);
+        int playerX = playerLocation[0];
+        int playerY = playerLocation[1];
+        int ghost1X = ghost1location[0];
+        int ghost1Y = ghost1location[1];
+        int ghost2X = ghost2location[0];
+        int ghost2Y = ghost2location[1];
+        if (getNumGhosts() > 2) {
+            int ghost3X = ghost3location[0];
+            int ghost3Y = ghost3location[1];
+            int ghost4X = ghost4location[0];
+            int ghost4Y = ghost4location[1];
+            if (playerX == ghost3X && playerY == ghost3Y) {
+                ghostCollision = true;
+            }
+            if (playerX == ghost4X && playerY == ghost4Y) {
+                ghostCollision = true;
+            }
+        }
+        if (playerX == ghost1X && playerY == ghost1Y) {
+            ghostCollision = true;
+        }
+        if (playerX == ghost2X && playerY == ghost2Y) {
+            ghostCollision = true;
+        }
+        if (ghostCollision) {
+            gameOver();
+        }
+        if (elapsed >= 100) {
+            previousPlayerTime = now;
+            movePlayer();
+            setStatus(getCakesEaten() + "");
+        }
+
+        //Stop at 200 for testing without eating all cakes
+        if (getCakesRemaining() == 200) {
+            nextLevel();
+        }
+    }
+
+    private void gameOver() {
+        setPaused(true);
+        playerTimer.stop();
+        ghostTimer.stop();
+        System.out.println("You hit a ghost!");
+
     }
 
     private void createGhosts() {
@@ -326,7 +574,7 @@ public class jbergerAshman extends Application {
     private void onAbout() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("About");
-        alert.setHeaderText("Jacob Berger, CSCD370 Lab Template, Winter 2020");
+        alert.setHeaderText("Jacob Berger, CSCD370 Ashman, Winter 2020");
         alert.showAndWait();
     }
 
@@ -363,17 +611,13 @@ public class jbergerAshman extends Application {
         Paint paint;
         switch (tile) {
             case EMPTY:
-                paint = Color.WHITE;
-                rectangle.setFill(paint);
-                getNodeAtPlayer().setImage(new Image(String.valueOf(rectangle)));
+                getNodeAtPlayer().setImage(new Image("Images/Empty.png"));
                 break;
             case CAKE:
                 getNodeAtPlayer().setImage(new Image("Images/Cake.png", 20, 20, false, false));
                 break;
             case WALL:
-                paint = Color.BLACK;
-                rectangle.setFill(paint);
-                getNodeAtPlayer().setImage(new Image(String.valueOf(rectangle)));
+                getNodeAtPlayer().setImage(new Image("Images/Wall"));
                 break;
         }
     }
@@ -392,7 +636,5 @@ public class jbergerAshman extends Application {
             return false;
         }
     }
-
-
 
 }
